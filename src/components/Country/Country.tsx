d7eab561b4c8390f4e5e5e1d98937fdc1e78b902
officialbondondatta <dattabondon1320@gmail.com>
2026-09-11 16:02:14 +0600

    added functional visited button

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { CountryType } from "../../types";
 import './Country.css'
 export interface CountryProps {
@@ -5,14 +6,25 @@ export interface CountryProps {
 }
 
 export default function Country({ country }: CountryProps) {
+    const [visited, setVisited] = useState<boolean>(false)
+    const handleVisited = () => {
+        // setVisited(true)
+        // if (visited) {
+        //     setVisited(false)
+        // }
+        // else {
+        //     setVisited(true)
+        // }
+        setVisited(!visited)
+    }
 
     return (
-        <div className="country">
+        <div className={`country ${visited ? "country-visited" : ""}`}>
             <h1>{country.name.common}</h1>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <p>Capital: {country.capital.capital.join(", ")}</p>
             <p>Population: {country.population.population}</p>
-            <button>Visited</button>
+            <button onClick={handleVisited}>{visited ? "Visited" : "Mark As Visited"}</button>
         </div>
     )
 }
